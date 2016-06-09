e3411d1fc4de8aeeedc5b7fc7f0454d7e0759e8a
Zakaria Boualaid <boulaidzac@gmail.com>
2016-06-09 21:40:51 +0000

    Fix seed for prod

--- traincommander_api/db/seeds.rb
+++ traincommander_api/db/seeds.rb
@@ -2,17 +2,19 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-
+  Train.delete_all
   trains = Train.create([{id: 1,name: "T1", city: "Casablanca", capacity: 300},
     {id: 2, name: "T2", city: "Rabat", capacity: 320},
     {id: 3, name: "T3", city: "Marrakech", capacity: 230},
     {id: 4, name: "T4", city: "Agadir", capacity: 220}])
 
+  Station.delete_all
   stations = Station.create([{id: 1, name: "Casa Voyageur"},
     {id: 2, name: "L'Oasis"},
     {id: 3, name: "Rabat Agdal"}
     ]);
 
+  Trip.delete_all
   trips = Trip.create([{id:1, departure_time: "08/06/2016 00:00:00", total_travel_time: 62315, price: 220, train_id: 1, from_id: 1, to_id: 3},
     {id: 2, departure_time: "08/06/2016 00:00:00", total_travel_time: 33689, price: 220, train_id: 1, from_id: 2, to_id: 3},
     {id: 3, departure_time: "09/06/2016 12:30:00", total_travel_time: 62315, price: 220, train_id: 1, from_id: 3, to_id: 1}
